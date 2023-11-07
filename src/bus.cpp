ccb66554fb7f207534e8ff6e2981c3cdb9e1ebc4
Amadeus Winarto <amadeusw6@gmail.com>
2023-11-07 20:58:14 +0800

    Fix bus acquire-release

--- src/bus.cpp
+++ src/bus.cpp
@@ -1,4 +1,5 @@
 #include "bus.hpp"
+#include <iostream>
 
 Bus::Bus(int num_processors)
     : response_valid_bits(num_processors, false),
@@ -6,31 +7,52 @@ Bus::Bus(int num_processors)
       response_wait_bits(num_processors, false){};
 
 auto Bus::acquire(int controller_id) -> int {
-  // Register if not already registered
-  bool is_registered{false};
-  for (const auto &id : registration_queue) {
-    if (id == controller_id) {
-      is_registered = true;
-      break;
-    }
-  }
-  if (!is_registered) {
-    registration_queue.push_back(controller_id);
-  }
-
   if (!owner_id) {
-    owner_id = registration_queue.front();
-    registration_queue.pop_front();
-  }
+    // Not owner -> make yourself owner
+    owner_id = controller_id;
+    return true;
+  } else if (owner_id && owner_id.value() == controller_id) {
+    // Got owner and I'm owner
+    return true;
+  } else {
+    // Put yourself in queue, if you're not already in the queue
+    auto is_in_queue{false};
+    for (const auto x : registration_queue) {
+      if (x == controller_id) {
+        is_in_queue = true;
+        break;
+      }
+    }
 
-  return owner_id == controller_id;
+    if (!is_in_queue) {
+      registration_queue.push_back(controller_id);
+    }
+
+    return false;
+  }
 }
 
 void Bus::release(int controller_id) {
+  // Release bus if owner
   if (owner_id && owner_id.value() == controller_id) {
     owner_id = std::nullopt;
+
+    // Get next owner if any
+    if (!registration_queue.empty()) {
+      owner_id = registration_queue.front();
+      registration_queue.pop_front();
+    }
+  } else if (!owner_id) {
+    std::cout << "Error: Bus is not owned by anyone but Core " << controller_id
+              << " wants to release. " << std::endl;
+    std::exit(1);
+  } else if (owner_id && owner_id.value() != controller_id) {
+    std::cout << "Error: Bus is owned by " << owner_id.value() << " but core "
+              << controller_id << " wants to release. " << std::endl;
+    std::exit(1);
   }
 
+  // std::cout << "Bus released by " << controller_id << std::endl;
   already_flush = false;
 }
 
