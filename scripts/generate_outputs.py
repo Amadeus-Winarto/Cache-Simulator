7803b526312d307592b0701fa4e5365eb270f9d0
Amadeus Winarto <amadeusw6@gmail.com>
2023-11-22 00:54:49 +0800

    Support compilation

--- scripts/generate_outputs.py
+++ scripts/generate_outputs.py
@@ -5,6 +5,11 @@ import multiprocessing
 import os
 from tqdm import tqdm
 
+RECOMPILE = False
+DEBUG = False
+USE_WRITE_BUFFER = False
+
+
 PROGRAM = "build/coherence"
 TEST_DIR = "tests"
 PROTOCOLS = ["MESI", "MOESI", "Dragon", "MESIF"]
@@ -20,6 +25,46 @@ OUTPUT_DIR = "outputs/refactored"
 
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
+
+if RECOMPILE:
+    flags = []
+    if DEBUG:
+        flags.append("-DDEBUG=ON")
+    if USE_WRITE_BUFFER:
+        flags.append("-DUSE_WRITE_BUFFER=ON")
+
+    flag_str = " ".join(flags)
+
+    if len(flags) > 0:
+        print("Recompiling with flags: ", flag_str)
+    else:
+        print("Recompiling...")
+
+    subprocess.run(
+        "rm -rf build",
+        shell=True,
+        check=True,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    os.makedirs("build")
+    subprocess.run(
+        f"cmake {flag_str} -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -Bbuild -G Ninja",
+        shell=True,
+        check=True,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+
+    subprocess.run(
+        "cmake --build build ",
+        shell=True,
+        check=True,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+
+    print("Done compiling")
 
 
 def run_testcase(protocol_testcase_pair):
